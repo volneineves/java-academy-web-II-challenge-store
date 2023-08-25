a4943073c48c2ab4c379dae377b826a39a618284
Volnei Neves <volneinevesfilho@hotmail.com>
2023-08-25 19:25:12 -0300

    "fix(application, service): fixing variables by jwt and feign client"

--- src/main/java/com/ada/avanadestore/service/JwtService.java
+++ src/main/java/com/ada/avanadestore/service/JwtService.java
@@ -4,6 +4,7 @@ import com.ada.avanadestore.dto.TokenDTO;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +14,17 @@ import java.util.Date;
 @Service
 public class JwtService {
 
+    @Value("${jwt.expTime}")
+    private String expTime;
+
     public TokenDTO generateToken(UserDetails userDetails) {
+
+        Date actualDate = new Date();
+        Date expDate = new Date(actualDate.getTime() + Long.parseLong(expTime));
+
         String token = "Bearer " + Jwts.builder().setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setIssuedAt(actualDate)
+                .setExpiration(expDate)
                 .signWith(getSigningKey(), SignatureAlgorithm.HS512).compact();
 
         return new TokenDTO("access_token", token);
