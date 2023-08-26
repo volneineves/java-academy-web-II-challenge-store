d8b76785bf4d7103e5b49fb90857105183a41879
Volnei Neves <volneinevesfilho@hotmail.com>
2023-08-26 14:35:11 -0300

    refactor(handler): updating exception handler logs and class with annotation @RestControllerAdvice

--- src/main/java/com/ada/avanadestore/handler/CustomExceptionHandler.java
+++ src/main/java/com/ada/avanadestore/handler/CustomExceptionHandler.java
@@ -9,21 +9,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class CustomExceptionHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomExceptionHandler.class);
 
     @ExceptionHandler
     public ResponseEntity<StandardErrorDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception, HttpServletRequest request) {
-        LOGGER.error("MethodArgumentNotValidException: {}", exception.getMessage(), exception);
+        LOGGER.error("{}: {}", exception.getClass(), exception.getMessage());
         List<String> errorMessages = new ArrayList<>();
         for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
             String message = fieldError.getDefaultMessage();
@@ -36,7 +36,7 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<StandardErrorDTO> handleBadCredentialException(BadCredentialsException exception, HttpServletRequest request) {
-        LOGGER.error("BadCredentialsException: {}", exception.getMessage(), exception);
+        LOGGER.error("{}: {}", exception.getClass(), exception.getMessage());
         StandardErrorDTO error = new StandardErrorDTO(HttpStatus.FORBIDDEN.value(), exception.getMessage(), request.getRequestURI(), request.getMethod(), new Date().toString());
         return ResponseEntity.status(error.status()).body(error);
     }
