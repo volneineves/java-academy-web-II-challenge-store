33b7101ef9abbc1c6f591db25fe47e805efd08b7
Volnei Neves <volneinevesfilho@hotmail.com>
2023-08-26 19:39:38 -0300

    refactor(handler): updating customExceptionHandler methods

--- src/main/java/com/ada/avanadestore/handler/CustomExceptionHandler.java
+++ src/main/java/com/ada/avanadestore/handler/CustomExceptionHandler.java
@@ -1,50 +1,70 @@
 package com.ada.avanadestore.handler;
 
 import com.ada.avanadestore.dto.StandardErrorDTO;
+import com.ada.avanadestore.exception.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomExceptionHandler.class);
 
-    @ExceptionHandler
-    public ResponseEntity<StandardErrorDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception, HttpServletRequest request) {
-        LOGGER.error("{}: {}", exception.getClass(), exception.getMessage());
-        List<String> errorMessages = new ArrayList<>();
-        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
-            String message = fieldError.getDefaultMessage();
-            errorMessages.add(message);
-        }
-
-        StandardErrorDTO error = new StandardErrorDTO(HttpStatus.BAD_REQUEST.value(), String.join("; ", errorMessages), request.getRequestURI(), request.getMethod(), new Date().toString());
-        return ResponseEntity.status(error.status()).body(error);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardErrorDTO> handleInvalidMethodArguments(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        List<String> errorMessages = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.toList());
+        return buildErrorResponse(ex, request, HttpStatus.BAD_REQUEST, String.join("; ", errorMessages));
     }
 
-    @ExceptionHandler
-    public ResponseEntity<StandardErrorDTO> handleBadCredentialException(BadCredentialsException exception, HttpServletRequest request) {
-        LOGGER.error("{}: {}", exception.getClass(), exception.getMessage());
-        StandardErrorDTO error = new StandardErrorDTO(HttpStatus.FORBIDDEN.value(), exception.getMessage(), request.getRequestURI(), request.getMethod(), new Date().toString());
-        return ResponseEntity.status(error.status()).body(error);
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<StandardErrorDTO> handleBadCredentials(BadCredentialsException ex, HttpServletRequest request) {
+        return buildErrorResponse(ex, request, HttpStatus.FORBIDDEN);
     }
 
-    @ExceptionHandler
-    public ResponseEntity<StandardErrorDTO> handleNullPointerException(NullPointerException exception, HttpServletRequest request) {
-        LOGGER.error("{}: {}", exception.getClass(), exception.getMessage());
-        StandardErrorDTO error = new StandardErrorDTO(HttpStatus.BAD_REQUEST.value(), exception.getMessage(), request.getRequestURI(), request.getMethod(), new Date().toString());
-        return ResponseEntity.status(error.status()).body(error);
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<StandardErrorDTO> handleNullPointerException(NullPointerException ex, HttpServletRequest request) {
+        return buildErrorResponse(ex, request, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<StandardErrorDTO> handleUnreadableMessage(HttpMessageNotReadableException ex, HttpServletRequest request) {
+        return buildErrorResponse(ex, request, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardErrorDTO> handleResourceNotFound(ResourceNotFoundException ex, HttpServletRequest request) {
+        return buildErrorResponse(ex, request, HttpStatus.NOT_FOUND);
+    }
+
+    private ResponseEntity<StandardErrorDTO> buildErrorResponse(Exception ex, HttpServletRequest request, HttpStatus status) {
+        return buildErrorResponse(ex, request, status, ex.getMessage());
+    }
+
+    private ResponseEntity<StandardErrorDTO> buildErrorResponse(Exception ex, HttpServletRequest request, HttpStatus status, String message) {
+        LOGGER.error("{}: {}", ex.getClass().getName(), ex.getMessage());
+        StandardErrorDTO error = new StandardErrorDTO(
+                status.value(),
+                message,
+                request.getRequestURI(),
+                request.getMethod(),
+                new Date().toString()
+        );
+        return ResponseEntity.status(status).body(error);
     }
 }
