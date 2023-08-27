3f2479b20c6512adaf4f85b1cb2547d56ac30fa4
Volnei Neves <volneinevesfilho@hotmail.com>
2023-08-27 17:46:45 -0300

    fix(handler): fix last commit with entryPointHandler

---
+++ src/main/java/com/ada/avanadestore/handler/CustomAuthenticationEntryPointHandler.java
@@ -0,0 +1,32 @@
+package com.ada.avanadestore.handler;
+
+import com.ada.avanadestore.dto.StandardErrorDTO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.security.SignatureException;
+import java.util.Date;
+
+import static com.ada.avanadestore.constants.Messages.AUTHENTICATION_INVALID;
+
+@Component
+public class CustomAuthenticationEntryPointHandler implements AuthenticationEntryPoint {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomExceptionHandler.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        LOGGER.error("{}: {}", authException.getClass(), authException.getMessage());
+        StandardErrorDTO error = new StandardErrorDTO(401, AUTHENTICATION_INVALID, request.getRequestURI(), request.getMethod(), new Date().toString());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.getWriter().write(error.toString());
+    }
+}
